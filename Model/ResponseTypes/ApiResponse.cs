c1c0688b79f6775009da1d6d9614ea9bf4ffe8c1
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-10-15 00:07:09 +0300

    service response && api response added to response types

---
+++ Model/ResponseTypes/ApiResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace Model.ResponseTypes
+{
+    public class ApiResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        private ApiResponse(T? data, bool success, string? message)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+        }
+
+        public static ApiResponse<T> Ok(T data, string? message = null)
+        {
+            return new ApiResponse<T>(data, true, message);
+        }
+
+        public static ApiResponse<T> Fail(string message)
+        {
+            return new ApiResponse<T>(default, false, message);
+        }
+
+        public static ApiResponse<T> OkMessage(string message)
+        {
+            return new ApiResponse<T>(default, true, message);
+        }
+    }
+}
---
+++ Model/ResponseTypes/ServiceResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace Model.ResponseTypes
+{
+    public class ServiceResponse<T>
+    {
+        public T? Data { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+
+        private ServiceResponse(T? data, bool success, string? message)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+        }
+
+        public static ServiceResponse<T> Ok(T data, string? message = null)
+        {
+            return new ServiceResponse<T>(data, true, message);
+        }
+
+        public static ServiceResponse<T> Fail(string message)
+        {
+            return new ServiceResponse<T>(default, false, message);
+        }
+
+        public static ServiceResponse<T> OkMessage(string message)
+        {
+            return new ServiceResponse<T>(default, true, message);
+        }
+    }
+}
