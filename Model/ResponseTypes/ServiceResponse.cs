aec1f570a5878eb6faff64ab2cb8d41bf124e60c
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-10-15 00:09:46 +0300

    service response refactored

--- Model/ResponseTypes/ServiceResponse.cs
+++ Model/ResponseTypes/ServiceResponse.cs
@@ -6,6 +6,8 @@
         public bool Success { get; set; }
         public string? Message { get; set; }
 
+        public bool IsOperational { get; set; } // default false -> means there is system exception do not expose to client but write to system logs
+
         private ServiceResponse(T? data, bool success, string? message)
         {
             Data = data;
@@ -13,14 +15,22 @@
             Message = message;
         }
 
+        private ServiceResponse(T? data, bool success, string? message, bool isOperational)
+        {
+            Data = data;
+            Success = success;
+            Message = message;
+            IsOperational = isOperational;
+        }
+
         public static ServiceResponse<T> Ok(T data, string? message = null)
         {
             return new ServiceResponse<T>(data, true, message);
         }
 
-        public static ServiceResponse<T> Fail(string message)
+        public static ServiceResponse<T> Fail(string message, bool isOperational = false)
         {
-            return new ServiceResponse<T>(default, false, message);
+            return new ServiceResponse<T>(default, false, message, isOperational);
         }
 
         public static ServiceResponse<T> OkMessage(string message)
