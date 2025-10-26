085ad7b39cfcdc7897fc2555c243f370c1b46d0e
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-10-26 11:23:32 +0300

    path util added

--- Icoaura/Util/PathUtil.cs
+++ Icoaura/Util/PathUtil.cs
@@ -1,13 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Icoaura.Util
 {
-    class PathUtil
+    public class PathUtil
     {
 
+        // %APPDATA%/test/util.exe -> C:/Users/username/AppData/Roaming/test/util.exe
+        public static string Resolve(string path)
+        {
+            List<string> specialFolders = EnvUtil.SpecialFolders;
+            List<string> resolvedSpecialFolders = specialFolders
+                .Select(path_ => Environment.ExpandEnvironmentVariables(path_))
+                .ToList();
+
+
+            foreach (string speialFolder in specialFolders)
+            {
+                if (path.StartsWith(speialFolder))
+                {
+                    int indexOf = specialFolders.IndexOf(speialFolder);
+                    string absoluteDirPath = resolvedSpecialFolders[indexOf];
+                    path = absoluteDirPath + path.Substring(speialFolder.Length);
+                    return path;
+                }
+            }
+
+
+            return path;
+        }
+
+
+        // C:/Users/username/AppData/Roaming/test/util.exe -> %APPDATA%/test/util.exe
+        public static string UnResolve(string path)
+        {
+            List<string> specialFolders = EnvUtil.SpecialFolders;
+            List<string> resolvedSpecialFolders = specialFolders
+                .Select(path_ => Environment.ExpandEnvironmentVariables(path_))
+                .ToList();
+
+            string longestPrefix = string.Empty;
+            foreach (string resolvedSpecialFolder in resolvedSpecialFolders)
+            {
+                if (path.StartsWith(resolvedSpecialFolder) && resolvedSpecialFolder.Length > longestPrefix.Length)
+                {
+                    longestPrefix = resolvedSpecialFolder;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(longestPrefix))
+            {
+                int indexOf = resolvedSpecialFolders.IndexOf(longestPrefix);
+                string specialFolder = specialFolders[indexOf];
+                path = specialFolder + path.Substring(longestPrefix.Length);
+
+                return path;
+            }
+
+            return path;
+        }
+
+
+        public static string ConvertToRelativePath(string path)
+        {
+            List<string> specialFolders = EnvUtil.SpecialFolders;
+            List<string> resolvedSpecialFolders = specialFolders
+                .Select(path_ => Environment.ExpandEnvironmentVariables(path_))
+                .ToList();
+
+            string longestPrefix = string.Empty;
+            foreach (string resolvedSpecialFolder in resolvedSpecialFolders)
+            {
+                if (path.StartsWith(resolvedSpecialFolder) && resolvedSpecialFolder.Length > longestPrefix.Length)
+                {
+                    longestPrefix = resolvedSpecialFolder;
+                }
+            }
+
+            if (!string.IsNullOrEmpty(longestPrefix))
+            {
+                int indexOf = resolvedSpecialFolders.IndexOf(longestPrefix);
+                string specialFolder = specialFolders[indexOf];
+                path = specialFolder + path.Substring(longestPrefix.Length);
+
+                // add wildcard * after special path
+                path = string.Join("\\", path.Split("\\").Select((item, index) =>
+                {
+                    if (index == 0 || index == path.Split("\\").Length - 1) return item;
+                    return "**";
+                }).ToList());
+
+                return path;
+            }
+
+
+            return path;
+        }
+
     }
+
 }
