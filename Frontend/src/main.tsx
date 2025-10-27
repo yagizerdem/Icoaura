a6e74efdb8d6125787ad7004f47c83ac930d7935
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-10-28 00:52:58 +0300

    update

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./flash.css";
 import "./index.css";
+import "toastify-js/src/toastify.css";
 import App from "./App.tsx";
 import { BaseProvider } from "./Providers/BaseProvider.tsx";
 
