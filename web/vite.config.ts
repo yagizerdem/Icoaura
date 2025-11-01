96a84a945074976212a8bd6a11710cd05774f09e
yagizerdem <126137803+yagizerdem@users.noreply.github.com>
2025-11-01 19:07:50 +0300

    vite base url added

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./",
+  base: "Icoaura",
   plugins: [react(), tailwindcss()],
 });
